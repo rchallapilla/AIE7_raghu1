d5e361981791105fe722fef100d0b693b24215e7
Raghu Challapilla <raghu.challapilla@alumni.ie.edu>
2025-06-15 17:58:22 -0400

    I added my name and location info

--- 00_Onramp/onramp01_Cursor_and_Git/main.py
+++ 00_Onramp/onramp01_Cursor_and_Git/main.py
@@ -4,13 +4,13 @@ print("Hello, World!")
 #### ❓Question #1:
 # What is your name?
 print("What is your name?")
-name = # Add your name here
+name = "Raghu Challapilla"  # Replace with your actual name
 print("Hello, " + name + "!")
 
 #### ❓Question #2:
 # Where do you live?
 print("Where do you live?")
-location = # Add your location here
+location = 'charlotte'
 print("I live in " + location + ".")
 
 
